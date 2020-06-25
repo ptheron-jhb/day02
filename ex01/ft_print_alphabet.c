07aa7d45a2033a7f2734c4fcd8c4c6b5bbb77154
ptheron-jhb <67001715+ptheron-jhb@users.noreply.github.com>
2020-06-25 19:47:03 +0200

    Add files via upload
    Amended code

---
+++ ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ptheron <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/17 12:28:54 by ptheron           #+#    #+#             */
+/*   Updated: 2020/06/17 12:30:18 by ptheron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+void ft_putchar(char a) {
+   	write(STDOUT_FILENO, &a, 1); 
+}
+
+void ft_print_alphabet(void) {
+	char ch;
+	ch = 'a';
+	while (ch <= 'z') {
+    ft_putchar(ch);
+    ch++;
+  }
+}
+
+int main(void) {
+  ft_print_alphabet();
+  return 0;
+}
